b6f2d1c5d42a2bc5dcf2f3d0966c2346d2459b6f
Frank Hunleth <fhunleth@troodon-software.com>
2026-05-30 14:39:18 -0400

    Fix preferred cli env warning

--- mix.exs
+++ mix.exs
@@ -23,13 +23,12 @@ defmodule OneDHCPD.MixProject do
       dialyzer: [
         flags: [:missing_return, :extra_return, :unmatched_returns, :error_handling, :underspecs]
       ],
-      deps: deps(),
-      preferred_cli_env: %{
-        docs: :docs,
-        "hex.publish": :docs,
-        "hex.build": :docs
-      }
+      deps: deps()
     ]
+  end
+
+  def cli do
+    [preferred_envs: %{docs: :docs, "hex.publish": :docs, "hex.build": :docs}]
   end
 
   def application do
